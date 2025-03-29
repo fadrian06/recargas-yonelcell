812b1c7a02957254647458714b2ed5debc2d6617
fadrian06 <franyeradriansanchez@gmail.com>
2025-03-29 16:06:52 -0400

    add Controller::renderPage(...)

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,21 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Flight;
+
 abstract readonly class Controller
 {
-  // ...
+  protected static function renderPage(
+    string $page,
+    string $title,
+    string $layout,
+    array $data = []
+  ): never {
+    $data['title'] ??= $title;
+
+    Flight::render("pages/$page", $data, 'content');
+    Flight::render("layouts/$layout", $data);
+
+    exit;
+  }
 }
