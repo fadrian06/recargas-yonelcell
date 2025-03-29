86fef7df171f9add4104912cb5434838d0c491aa
fadrian06 <franyeradriansanchez@gmail.com>
2025-03-29 15:09:56 -0400

    update faslatam/flighravel -> v1.7.0

--- scripts/config.mjs
+++ scripts/config.mjs
@@ -1,5 +1,6 @@
 import { copyFileSync, existsSync, readFileSync, writeFileSync } from 'node:fs'
 import { parse } from 'dotenv'
+// import { sassPlugin } from 'esbuild-sass-plugin'
 
 /** @type {import('esbuild').BuildOptions} */
 export const commonOptions = {
@@ -17,6 +18,11 @@ export const commonOptions = {
   outdir: 'resources/dist',
   target: ['es2018'],
   conditions: ['main'],
+  plugins: [
+    /*sassPlugin({
+      quietDeps: true,
+    }),*/
+  ],
 }
 
 if (!existsSync('.env')) {
